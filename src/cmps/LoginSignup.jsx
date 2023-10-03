2021a8a3597fb9325ba783a39912e3dc42018ae5
eliormangistu <eliormangistu@gmail.com>
2023-10-04 02:05:52 +0300

    log-in-page

--- src/cmps/LoginSignup.jsx
+++ src/cmps/LoginSignup.jsx
@@ -47,51 +47,59 @@ export function LoginSignup(props) {
     }
 
     return (
-        <div className="login-page">
-            <p>
-                <button className="btn-link" onClick={toggleSignup}>{!isSignup ? 'Signup' : 'Login'}</button>
-            </p>
-            {!isSignup && <form className="login-form" onSubmit={onLogin}>
-                <select
-                    name="username"
-                    value={credentials.username}
-                    onChange={handleChange}
-                >
-                    <option value="">Select User</option>
-                    {users.map(user => <option key={user._id} value={user.username}>{user.fullname}</option>)}
-                </select>
-
-                <button>Login!</button>
-            </form>}
-            <div className="signup-section">
-                {isSignup && <form className="signup-form" onSubmit={onSignup}>
-                    <input
-                        type="text"
-                        name="fullname"
-                        value={credentials.fullname}
-                        placeholder="Fullname"
-                        onChange={handleChange}
-                        required
-                    />
-                    <input
-                        type="text"
+        <div className="login-container flex items-center">
+            <img
+                src="src\assets\images\iphone-with-profile.jpg" alt="Instagram"
+            />
+            <section className='login-form'>
+                <img
+                    src='src\assets\images\logo.png' alt='logo'>
+                </img>
+                {/* <p>
+                    <button className="btn-link" onClick={toggleSignup}>{!isSignup ? 'Signup' : 'Login'}</button>
+                </p> */}
+                {!isSignup && <form className="login-form" onSubmit={onLogin}>
+                    <select
                         name="username"
                         value={credentials.username}
-                        placeholder="Username"
                         onChange={handleChange}
-                        required
-                    />
-                    <input
-                        type="password"
-                        name="password"
-                        value={credentials.password}
-                        placeholder="Password"
-                        onChange={handleChange}
-                        required
-                    />
-                    <button >Signup!</button>
+                    >
+                        <option value="">Select User</option>
+                        {users.map(user => <option key={user._id} value={user.username}>{user.fullname}</option>)}
+                    </select>
+
+                    <button>Login!</button>
                 </form>}
-            </div>
+                <div className="signup-section">
+                    {isSignup && <form className="signup-form" onSubmit={onSignup}>
+                        {/* <input
+                            type="text"
+                            name="fullname"
+                            value={credentials.fullname}
+                            placeholder="Fullname"
+                            onChange={handleChange}
+                            required
+                        /> */}
+                        <input className='sign-up-input'
+                            type="text"
+                            name="username"
+                            value={credentials.username}
+                            placeholder="Phone number, username, or email"
+                            onChange={handleChange}
+                            required
+                        />
+                        <input className='sign-up-input'
+                            type="password"
+                            name="password"
+                            value={credentials.password}
+                            placeholder="Password"
+                            onChange={handleChange}
+                            required
+                        />
+                        <button >Log in</button>
+                    </form>}
+                </div>
+            </section >
         </div>
     )
 }
